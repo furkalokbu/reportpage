790faef0180696057a8e843ec42fc94ff1191276
mechanic <furkalokbu@gmail.com>
2021-08-30 12:06:08 +0300

    change setting for different env

--- reportpage/prod.py
+++ reportpage/prod.py
@@ -1,19 +1,12 @@
 from .base import *
 import os
-import dotenv
-
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-k((9-_0t&p2s+%j4s9qc#xmdz+9k9^vs17oy%tibozrj-bysve'
-SECRET_KEY = os.environ['SECRET_KEY_DEV']
+SECRET_KEY = ''
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -22,10 +15,10 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'reportpage_dev',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgresdb.cqh8zx545xp6.eu-central-1.rds.amazonaws.com',
+        'NAME': 'reportpage_prod',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '5432',
     }
 }
