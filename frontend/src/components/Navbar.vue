c618fa8675cc96618fa8954c0ee5f82fbfbece19
mechanic <furkalokbu@gmail.com>
2021-09-01 11:54:25 +0300

    add new vue component and response json on page

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light my-navbar">
+        <div class="container">
+             <router-link
+                :to="{ name: 'Home' }"
+                class="navbar-brand"
+                >ReportPage 
+            </router-link>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <router-link
+                        :to="{ name: 'Home' }"
+                        class="nav-link"
+                      >Home  
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link
+                        :to="{ name: 'Home' }"
+                        class="nav-link"
+                      >Add data  
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link
+                        :to="{ name: 'About' }"
+                        class="nav-link"
+                      >About  
+                    </router-link>
+                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/accounts/logout/">Logout</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default{
+    name: "NavbarComponent"
+}
+</script>
+
+<style>
+    .my-navbar {
+        border-bottom: 1px solid #DDD;
+    }
+    .navbar-brand {
+        font-weight: bold;
+        font-size: 130%;
+    }
+    .navbar-brand:hover {
+        color: #DC3545 !important
+    }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App!!!" />
+    <div class="container">
+      <div class="p-3">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Date</th>
+              <th scope="col">Distance</th>
+              <th scope="col">Duration</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(sp, index) in speed"
+              :key="sp.pk"
+              :index="index">
+              <th scope="row">{{ index + 1 }}</th>
+              <td>{{ sp.date }}</td>
+              <td>{{ sp.distance }}</td>
+              <td>{{ sp.duration }}</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+  <div>
+    <p>Average Speed:</p>
+  </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// import { apiService } from "../common/api.service"
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      speed: [],
+    }
   },
-};
+  methods: {
+    // getSpeedData() {
+    //   let endpoint = "api/speed/";
+    //   apiService(endpoint)
+    //         .then(data => {
+    //         this.speed.push(...data.results)
+    //       })
+    // }
+  },
+  async created() {
+    var response = await fetch('http://127.0.0.1:8000/api/speed/');
+    this.speed = await response.json();
+  },};
 </script>
